23e9f471c40c5e0156861e564d2c23d74a63a8aa
Lucas Van Hooste <lucas.van.hooste@supportsquare.io>
2022-09-02 19:09:05 +0200

    Renamed assemblies and put UnityOptionDrawer under separate namespace

--- Scripts/Editor/UnityOptionDrawer.cs
+++ Scripts/Editor/UnityOptionDrawer.cs
@@ -5,17 +5,20 @@ using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
-[CustomPropertyDrawer(typeof(UnityOption<>))]
-public class UnityOptionDrawer : PropertyDrawer
+namespace LVH.UnityOption.Editor
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(UnityOption<>))]
+    public class UnityOptionDrawer : PropertyDrawer
     {
-        EditorGUI.PropertyField(position, property.FindPropertyRelative("_value"), new GUIContent($"({property.displayName})", "This field is optional."), true);
-    }
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("_value"), new GUIContent($"({property.displayName})", "This field is optional."), true);
+        }
 
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-    {
-        int propertyCount = property.FindPropertyRelative("_value").CountInProperty();
-        return propertyCount * EditorGUIUtility.singleLineHeight + (propertyCount - 1) * EditorGUIUtility.standardVerticalSpacing;
-    }
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            int propertyCount = property.FindPropertyRelative("_value").CountInProperty();
+            return propertyCount * EditorGUIUtility.singleLineHeight + (propertyCount - 1) * EditorGUIUtility.standardVerticalSpacing;
+        }
+    } 
 }
